afeac21687e6021cfc816b03b84b2cb68e8c5ab8
mattdavis0351 <mattdavis0351@github.com>
2020-08-10 14:15:31 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+  const token = core.getInput("token");
+  const labels = core.getInput("labels").split(",");
+  const repos = core.getInput("repos");
+  const projectName = core.getInput("projectName");
+  const octokit = github.getOctokit(token);
+  const ctx = github.context;
+
+  try {
+    const query = `query issuesWithLabels($label: [String!]){ 
+        viewer { 
+          issues(labels: $label, first: 50) {
+            edges {
+              node {
+                title,
+                repository{
+                  nameWithOwner
+                }
+                
+              }
+            }
+          }
+        }
+      }`;
+    const vars = {
+      labels: label,
+    };
+    const issuesQuery = await octokit.graphql(query, vars);
+    core.info(issuesQuery);
+  } catch (error) {
+    core.debug(error);
+  }
+}
+
+run();
