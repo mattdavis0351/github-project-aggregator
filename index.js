491db436a70fd25c0eacfc6210c0ad7d43e638da
mattdavis0351 <mattdavis0351@github.com>
2020-08-10 15:39:35 -0700

    agian

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const { context } = require("@actions/github/lib/utils");
 
 async function run() {
   const token = core.getInput("token");
-  const labels = core
-    .getInput("labels")
-    .split(",")
-    .map((i) => {
-      return i.trim();
-    });
-  const repos = core.getInput("repos");
-  const projectName = core.getInput("projectName");
+  //   const labels = core
+  //     .getInput("labels")
+  //     .split(",")
+  //     .map((i) => {
+  //       return i.trim();
+  //     });
+  //   const repos = core.getInput("repos");
+  //   const projectName = core.getInput("projectName");
   const octokit = github.getOctokit(token);
   const ctx = github.context;
 
